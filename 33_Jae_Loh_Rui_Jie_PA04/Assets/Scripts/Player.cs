a6a2ab2cdd196f4df7d4f430159af0ca6c8c8969
Jaelapeno <jaeloh00@gmail.com>
2020-04-30 13:49:07 +0800

    Created Unity Project

---
+++ 33_Jae_Loh_Rui_Jie_PA04/Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 1;
+
+    private Rigidbody ridgidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ridgidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        ridgidbody.AddForce(movement * speed);
+    }
+}
